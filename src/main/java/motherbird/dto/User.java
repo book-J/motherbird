957a6978a77b8a234347f49b205cf42f50fae8e1
jinkyuhan <gkswlsrb95@gmail.com>
2020-06-02 02:54:20 +0900

    reformat gradle build script

--- src/main/java/motherbird/dto/User.java
+++ src/main/java/motherbird/dto/User.java
@@ -2,14 +2,11 @@ package motherbird.dto;
 
 public class User {
     private long id;
-
+    private String name;
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
-
 }
